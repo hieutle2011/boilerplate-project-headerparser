b82e0e39e66d833523d60e54914c4db71fa02a51
HIEU Le Trung <hieuletrung102@gmail.com>
2018-12-10 10:52:30 +0700

    Finish task

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", (req, res) => {
+  let ipaddress = req.ip
+  let language = req.acceptsLanguages()
+  let software = req.headers["user-agent"]
+  res.json({ipaddress: ipaddress, language: language, software: software})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
